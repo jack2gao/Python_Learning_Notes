3f39ff73180b3927ebdf0074fca59771fdb51004
Eric_G <cestmavie@live.cn>
2022-03-07 13:45:02 +0800

    0307

---
+++ Python基础/code/Python_HM_001.py
@@ -0,0 +1,13 @@
+"""
+作业001:求和赋值并打印
+
+Author: Jack2Gao
+Date: 220305
+"""
+
+if __name__ == "__main__":
+    Number1 = 1
+    Number2 = 2
+    result = Number1 + Number2
+    print("计算{} + {}的结果".format(Number1,Number2))
+    print("计算结果为: {}".format(result))
